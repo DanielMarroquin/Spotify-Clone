40f91f8297de3ea4ef3a83afafd9b3e5cfc7056d
Daniel <dev.danielmarroquin25@gmail.com>
2022-05-04 00:05:54 -0500

    add-new-changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { CardPlayerComponent } from './components/card-player/card-player.compon
 import { GenericSectionComponent } from './components/generic-section/generic-section.component';
 import { PlayListHeaderComponent } from './components/play-list-header/play-list-header.component';
 import { PlayListBodyComponent } from './components/play-list-body/play-list-body.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { PlayListBodyComponent } from './components/play-list-body/play-list-bod
     PlayListBodyComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     SideBarComponent,
