e8f9bc5ff4c1592cb2060d5ae0c8e62c3810d212
Daniel Marroquin <dev.danielmarroquin25@gmail.com>
2022-05-11 17:42:03 -0500

    add-new-changes

--- src/app/shared/directives/img-broken.directive.ts
+++ src/app/shared/directives/img-broken.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Element } from "@angular/compiler";
 
 @Directive({
   selector: '[appImgBroken]'
 })
 export class ImgBrokenDirective {
+  @HostListener('error') handleError(): void {
+    const elementNative = this.hostDirective.nativeElement;
+    elementNative.src = '../../../assets/images/error-404.png';
+    console.log('Error en la imagen ==>', this.hostDirective);
+  }
 
-  constructor() { }
+  //TODO: HOST el huesped de la directiva
+  constructor(private hostDirective: ElementRef) {
+    console.log(hostDirective, 'date')
+  }
+
 
 }
