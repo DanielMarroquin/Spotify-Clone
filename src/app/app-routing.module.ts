051d1288ca3d397dff1129e4fdbf22f8a3960d66
Daniel <dev.danielmarroquin25@gmail.com>
2022-05-20 14:25:30 -0500

    add-guards-service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePagesComponent } from '@modules/home/pages/home-pages/home-pages.component';
 import { HeaderUserComponent } from './shared/components/header-user/header-user.component';
+import {SessionGuard} from "@core/guards/session.guard";
 
 const routes: Routes = [
   {
     path: 'auth', // TODO: Localhost:4200/
     loadChildren:() => import(`./modules/auth/auth.module`).then(m => m.AuthModule)
     // loadChildren apunta a la ruta del modulo/componente que se desee
-    // Se usa loadChildren para el concepto de carga perezosa 
+    // Se usa loadChildren para el concepto de carga perezosa
   },
   {
     path: '',
     component: HomePagesComponent,
-    loadChildren:() => import(`./modules/home/home.module`).then(m => m.HomeModule)
+    loadChildren:() => import(`./modules/home/home.module`).then(m => m.HomeModule),
+    canActivate: [SessionGuard]
   }
 ];
 
